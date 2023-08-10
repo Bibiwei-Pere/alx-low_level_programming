d8011e9c335875d1b91b09df294e68fbe3e9fba4
Bibiwei-Pere <bibiweipere@gmail.com>
2023-08-10 11:25:47 -0700

    Advanced tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
-  * main - Entry point for printf function
+  * main - Entry point for write function
   * Return: 1 (Standard error)
 */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
