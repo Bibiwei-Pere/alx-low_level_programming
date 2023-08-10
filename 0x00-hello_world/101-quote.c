561531934b7888be8bc2f02dfafd88ffe68e7b35
Bibiwei-Pere <bibiweipere@gmail.com>
2023-08-10 05:47:59 -0700

    Advanced task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+  * main - Entry point for printf function
+  * Return: 1 (Standard error)
+*/
+int main(void)
+{
+	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - contains function that prints c data types
+  * Return: Always 0
+*/
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
+	return (0);
+}
