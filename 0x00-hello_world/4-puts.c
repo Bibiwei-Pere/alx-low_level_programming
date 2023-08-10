389dac91b78fc92d57477b17049bd2753ff16c4e
Bibiwei-Pere <bibiweipere@gmail.com>
2023-08-10 01:43:54 -0700

    First few tasks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - contains put function
+ * return - Always 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
