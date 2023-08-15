d570d8c532501bc1c16a7a02d2e98e34938c8e13
Bibiwei-Pere <bibiweipere@gmail.com>
2023-08-15 10:16:35 -0700

    First task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - Entry point
  * Return: Always 0
 */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
