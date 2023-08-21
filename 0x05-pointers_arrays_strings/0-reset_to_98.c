33d2464e67274bcd8a3e045bf76c67aa7d9c3266
Bibiwei-Pere <bibiweipere@gmail.com>
2023-08-21 02:08:08 -0700

    First commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
   * reset_to_98 - updates pointer
@@ -7,14 +6,4 @@
 void reset_to_98(int *n)
 {
 	*n = 98;
-}
-int main(void)
-{
-    int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
 }
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 /**
   * swap_int - swaps pointers
@@ -8,16 +7,4 @@ void swap_int(int *a, int *b)
 {
 	*a = 42;
 	*b = 98;
-}
-int main(void)
-{
-    int a;
-    int b;
-
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
 }
